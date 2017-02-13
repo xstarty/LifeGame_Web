15530d8f288fbe2444c18422f0ec623ff2cd0ce3
diddykao <diddykao@DIDDYKAO-XP.inhouse.moneydj.com>
2017-02-13 11:11:48 +0800

    hide index.php

--- ci_application/config/config.php
+++ ci_application/config/config.php
@@ -36,7 +36,7 @@ $config['base_url'] = 'http://life88.ddns.net/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -53,7 +53,7 @@ $config['index_page'] = 'index.php';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol']	= 'AUTO';
 
 /*
 |--------------------------------------------------------------------------
