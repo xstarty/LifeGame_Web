15bf8e26b530b8d5e1d6c928a81c0c0c696d4161
diddykao <diddykao@DIDDYKAO-XP.inhouse.moneydj.com>
2017-03-06 10:52:39 +0800

    tmp

--- ci_application/models/charmodel.php
+++ ci_application/models/charmodel.php
@@ -1,1 +1,34 @@
 <?php
+if (! defined ( 'BASEPATH' ))
+	exit ( 'No direct script access allowed' );
+	class CharModel extends CI_Model {
+		function __construct() {
+			parent::__construct ();
+		}
+		function insert($account, $password) {
+			$this->db->insert ( "char", Array (
+					"account" => $account,
+					"password" => $password
+					) );
+		}
+		function checkCharExist($account) {
+			$this->db->select ( "COUNT(*) AS users" );
+			$this->db->from ( "char" );
+			$this->db->where ( "account", $account );
+			$query = $this->db->get ();
+			return $query->row ()->users > 0;
+		}
+		public function getChar($account) {
+			$this->db->select ( "*" );
+			$query = $this->db->get_where ( "char", Array (
+					"account" => $account
+					) );
+
+			if ($query->num_rows () > 0) {
+				return $query->row (); // 回傳第一筆
+			} else {
+				return null;
+			}
+		}
+	}
+	?>
